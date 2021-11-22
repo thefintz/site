d061a1ab18729f8c1e23f8157205a980bd591ee8
gmbueno <gmbueno1802@gmail.com>
2021-11-22 17:07:22 -0300

    added privacy policy

--- src/views/legal/PrivacyPolicy.vue
+++ src/views/legal/PrivacyPolicy.vue
@@ -9,8 +9,8 @@
         <h3 class="is-size-3"> Política de Privacidade da Fintz </h3>
         <h4 class="is-size-5"> Última atualização: 19 de novembro de 2021 </h4>
         <span>
-          <br><br>
-          Fintz International Corporation (”Fintz,” “nós”), uma
+          <br>
+          A Fintz (”Fintz,” “nós”), uma
           empresa de Florianópolis, opera o site disponível em
           https://fintz.com.br/ site (o “Site”) e a interface de
           programação de aplicativo (a "API Fintz" ou “Fintz API”) disponibilizada
@@ -80,16 +80,16 @@
             <li>Cookies e dados relativos ao uso dos Serviços </li>
             <li>Dados do usuário final </li>
           </ul>
-
+          <br>
           Nossos clientes podem usar a API Fintz para permitir que seus
           aplicativos de cliente se conectem ou acessem aplicativos e fontes
           de dados de terceiros, de modo que a Fintz pode receber e coletar
           certos dados fornecidos por ou por meio desses aplicativos e fontes
-          de dados de terceiros (coletivamente, "Fim Dados do usuário").
-
+          de dados de terceiros (coletivamente, "Dados do usuário final").
+          <br><br>
           Por exemplo, essas informações podem incluir dados do mercado
-          financeiro
-
+          financeiro.
+          <br><br>
           Podemos coletar e analisar dados e outras informações relacionadas
           ao fornecimento, uso e desempenho dos Serviços e sistemas e
           tecnologias relacionados (incluindo, sem limitação, Dados do Cliente
@@ -100,7 +100,7 @@
           aplicável, incluindo, sem limitação, para melhorar e aprimorar os
           Serviços e para outros fins de desenvolvimento, diagnóstico e
           correção em conexão com os Serviços e outras ofertas da Fintz.
-
+          <br><br>
           Podemos divulgar esses dados apenas de forma agregada ou de outra
           forma não identificada em conexão com seus negócios e serviços.
 
@@ -130,21 +130,24 @@
           Outras tecnologias de rastreamento usadas incluem beacons, tags e
           scripts que coletam e rastreiam os mesmos tipos de informações
           descritos acima, a fim de melhorar e analisar nossos Serviços.
-
+          <br><br>
           Seu navegador da web ou dispositivo móvel pode permitir que você
           recuse determinados cookies ou indique quando um cookie está sendo
           enviado; no entanto, se você não aceitar cookies, pode não conseguir
           usar algumas partes do nosso Serviço.
-
+          <br><br>
           Exemplos de cookies que usamos:
+          <ul class="list">
+          <li>Cookies de sessão: usados para operar e gerenciar suas sessões ao
+          usar os Serviços.</li>
+          <li>Cookies de preferência: usados para lembrar as preferências e
+          configurações do usuário para os Serviços.</li>
+          <li>Cookies de segurança: Usados para implementar e gerenciar protocolos
+          de segurança para os Serviços. </li>
+          </ul>
 
-          Cookies de sessão: usados para operar e gerenciar suas sessões ao
-          usar os Serviços.
-          Cookies de preferência: usados para lembrar as preferências e
-          configurações do usuário para os Serviços.
-          Cookies de segurança: Usados para implementar e gerenciar protocolos
-          de segurança para os Serviços.
-          Google Analytics
+          <br>
+          <h3 class="is-size-4">Google Analytics </h3>
 
           Podemos usar o Google Analytics, um fornecedor terceirizado de
           ferramentas analíticas ou um serviço terceirizado semelhante para
@@ -157,7 +160,7 @@
           visite: https://tools.google.com/dlpage/gaoptout.
 
           <br><br>
-          Acesso à informação por terceiros
+          <h3 class="is-size-4"> Acesso à informação por terceiros </h3>
 
           Podemos fornecer seus dados pessoais e dados de usuário final para
           nossas afiliadas e terceiros que usamos para nos ajudar a fornecer
@@ -166,7 +169,7 @@
           serviços para nós ou para nós, incluindo, sem limitação, manter
           nossas listas de clientes e enviar materiais de marketing em nosso
           nome, e disponibilizar Aplicativos do Cliente para você.
-
+          <br><br>
           Podemos divulgar e / ou transferir informações sobre você, incluindo
           seus Dados Pessoais e Dados de Usuário Final, em conexão com uma
           fusão, venda, reorganização, aquisição ou outra mudança de
@@ -175,18 +178,19 @@
           transações, as Informações Pessoais geralmente são um dos ativos
           comerciais transferidos, mas permanecerão sujeitas aos termos da
           Política de Privacidade da Fintz então aplicável.
-
+          <br><br>
           Exceto conforme estabelecido nesta Política de Privacidade e
           permitido por lei, não forneceremos seus dados a terceiros, a menos
           que você nos dê autoridade para isso, autoridade que pode ser dada
           por você ao fazer escolhas através dos Serviços para divulgar seus
           dados a terceiros especificados festas.
-
+          <br><br>
           Esperamos que quaisquer parceiros terceirizados aos quais você
           autorizou o acesso aos seus dados cumpram as políticas de privacidade
-          que eles fornecem ao coletar suas informações. Uso de Dados
+          que eles fornecem ao coletar suas informações.
 
           <br><br>
+          <h3 class="is-size-4"> Uso de Dados </h3>
           Você concorda que podemos usar seus Dados Pessoais, Dados de Usuário
           Final e Dados de Uso (e dados derivados deles) para os fins para os
           quais foram coletados; para operar, melhorar e / ou personalizar o
@@ -225,16 +229,17 @@
               governamental e quando acreditarmos razoavelmente que a lei exige
             </li>
           </ul>
-          Transferência de dados
+
           <br><br>
+          <h3 class="is-size-4"> Transferência de dados </h3>
 
           Suas informações, incluindo dados pessoais, podem ser transferidas
           e mantidas em computadores localizados fora de seu estado,
           província, país ou outra jurisdição governamental onde as leis de
           proteção de dados podem ser diferentes daquelas em sua jurisdição.
-          Se você estiver localizado fora dos Estados Unidos e optar por nos
+          Se você estiver localizado fora dos Brasil e optar por nos
           fornecer informações, observe que seus dados, incluindo Dados
-          Pessoais, podem ser transferidos para os Estados Unidos para
+          Pessoais, podem ser transferidos para os Brasil para
           processamento e armazenamento.
 
           Seu consentimento com esta Política de Privacidade, seguido do envio
@@ -243,34 +248,37 @@
           informações de acordo com esta Política de Privacidade.
 
           <br><br>
-          Divulgação de Dados
-          <br><br>
-          A Fintz pode divulgar seus dados conforme descrito acima. Ele também
+          <h3 class="is-size-4"> Divulgação de Dados </h3>
+          A Fintz pode divulgar seus dados conforme descrito acima. Ela também
           pode divulgar quaisquer de seus dados a terceiros quando acreditar de
           boa fé que tal ação é necessária:
-
-          Para cumprir uma obrigação legal, incluindo qualquer ordem judicial
+          <ul class="list">
+          <li>Para cumprir uma obrigação legal, incluindo qualquer ordem judicial
           ou solicitação de uma agência governamental, e quando acreditamos
-          razoavelmente que a lei exige
-          Para fazer cumprir os nossos Termos e Condições Fintz, Política de
-          Privacidade ou outras políticas aplicáveis
-          Para proteger e defender os direitos de propriedade da Fintz e outros
-          Para prevenir, detectar e investigar possíveis irregularidades em
+          razoavelmente que a lei exige </li>
+          <li>Para fazer cumprir os nossos Termos e Condições Fintz, Política de
+          Privacidade ou outras políticas aplicáveis </li>
+          <li>Para proteger e defender os direitos de propriedade da Fintz e outros </li>
+          <li>Para prevenir, detectar e investigar possíveis irregularidades em
           relação aos nossos Serviços ou outras ações que possam causar danos
           ou interferência (intencional ou não) em nossos direitos ou
           propriedade, outros usuários de nosso Serviço ou qualquer outra
-          pessoa que possa ser prejudicada por tais atividades
-          Para proteger a segurança pessoal dos usuários dos nossos Serviços
-          e outros
-          Para se proteger contra responsabilidade legal
-          Atualizando suas informações
+          pessoa que possa ser prejudicada por tais atividades </li>
+          <li>Para proteger a segurança pessoal dos usuários dos nossos Serviços
+          e outros </li>
+          <li>Para se proteger contra responsabilidade legal </li>
+          </ul>
+
+          <br><br>
+          <h3 class="is-size-4"> Atualizando suas informações </h3>
 
           Se você for um Cliente e a qualquer momento desejar acessar,
           modificar ou excluir seus Dados Pessoais, acesse sua Conta do
           Cliente, que permite visualizar, modificar e excluir determinados
           Dados Pessoais que coletamos.
 
-          Links para outros sites
+          <br><br>
+          <h3 class="is-size-4"> Links para outros sites </h3>
 
           Nossos serviços podem conter links para outros sites que não são
           operados por nós. Se você clicar em um link de terceiros, será
@@ -280,7 +288,8 @@
           políticas de privacidade ou práticas de quaisquer sites ou serviços
           de terceiros.
 
-          Privacidade das crianças
+          <br><br>
+          <h3 class="is-size-4"> Privacidade das crianças </h3>
 
           Nossos serviços não se destinam a menores de 13 anos. Não coletamos
           intencionalmente informações de identificação pessoal de menores de
@@ -288,7 +297,8 @@
           crianças sem a verificação do consentimento dos pais, tomaremos
           providências para remover essa informação de nossos registros.
 
-          Mudanças nesta política
+          <br><br>
+          <h3 class="is-size-4"> Mudanças nesta política </h3>
 
           Podemos fazer alterações em nossa Política de Privacidade de tempos
           em tempos. Quando ocorrerem mudanças, publicaremos uma versão
