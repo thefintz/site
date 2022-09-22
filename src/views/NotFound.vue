d9d25c7af3fdb176d88eac078dffd3f69a034cfd
gmbueno <gmbueno1802@gmail.com>
2022-09-22 11:06:08 -0300

    updated 404 page design

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,44 +1,60 @@
 <template>
-<div class="container is-fluid has-background-light">
-  <div class="is-flex is-flex-direction-column is-align-items-center is-justify-content-space-around">
-    <figure class="image">
-      <img src="../assets/logo.png" >
-    </figure>
+<div>
+  <NavbarComp/>
+  <div class="has-background-light is-justify-content-center">
+      <div class="has-text-centered columns">
+        <div class="column is-2"></div>
 
-    <div class="has-text-centered">
-      <h4 class="title is-size-4">
-        Essa página não foi encontrada!
-      </h4>
+        <div class="column is-8">
 
-      <p class="has-text-grey-dark">
-        Parece que não conseguimos encontrar a página que você está procurando.
-        <br>
-        Tente voltar a procurar a página que deseja, qualquer coisa entre em contato
-        <br>
-        conosco para relatar um problema.
-        <br>
-        <br>
-        Contato WhatsApp:
-        <br>
-        (41) 9 9187-5540
-      </p>
+          <div class="is-flex is-justify-content-center pt-3" style="height: 12rem">
+            <figure class="image">
+              <img src="../assets/logo.png" >
+            </figure>
+          </div>
+
+          <h4 class="title is-size-4">
+            Essa página não foi encontrada!
+          </h4>
+          <p class="has-text-grey-dark">
+            Parece que não conseguimos encontrar a página que você está procurando.
+          </p>
+          <p>
+            Tente voltar a procurar a página que deseja, qualquer coisa entre em contato conosco para relatar um problema.
+          </p>
+
+          <button class="my-5 button is-primary is-large" @click="$router.push('/')">
+            <span class="has-text-weight-bold p-6 has-text-white">
+              Voltar à página inicial
+            </span>
+          </button>
+
+          <div>
+            <a class="py-3" href="https://wa.me/554191875540">
+              <span class="my-auto has-text-primary">
+                Contato WhatsApp
+              </span>
+            </a>
+          </div>
+        </div>
+
+        <div class="column is-2"></div>
+      </div>
+
+      <br>
     </div>
-
-    <button class="button is-primary is-large" @click="$router.back()">
-      <span class="has-text-weight-bold p-6 has-text-white">
-        voltar para o site
-      </span>
-    </button>
-
-    <br>
-  </div>
 </div>
 </template>
 
 <script>
+import NavbarComp from '@/comps/NavbarComp'
 
 export default {
-  name: 'NotFound'
+  name: 'NotFound',
+
+  components: {
+    NavbarComp
+  }
 }
 </script>
 
